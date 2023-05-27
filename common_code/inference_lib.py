cc03921269d6b0f748caa5cc062db86e50a9ca0b
Gonsoomoon <gonsoomoon@gmail.com>
2023-05-27 13:05:12 +0000

    edit kor llm nb

--- common_code/inference_lib.py
+++ common_code/inference_lib.py
@@ -1,5 +1,6 @@
 import boto3
 import time
+import json
 
     
 def descirbe_endpoint(endpoint_name):
@@ -23,7 +24,7 @@ def descirbe_endpoint(endpoint_name):
 
 def invoke_inference(endpoint_name, prompt):
     '''
-    프롬프트를 제공하여 엔드포인트 호출
+    KoAlpaca 프롬프트를 제공하여 엔드포인트 호출
     '''
     client = boto3.client("sagemaker-runtime")
     
@@ -34,4 +35,34 @@ def invoke_inference(endpoint_name, prompt):
     #print(response["Body"].read())
     res = response["Body"].read().decode()
     print (eval(res)[0]['generated_text'])
+
             
+def query_endpoint_with_json_payload(encoded_json, endpoint_name, content_type="application/json"):
+    client = boto3.client("runtime.sagemaker")
+    response = client.invoke_endpoint(
+        EndpointName=endpoint_name, ContentType=content_type, Body=encoded_json
+    )
+    return response
+        
+        
+def query_endpoint_with_json_payload(encoded_json, endpoint_name, content_type="application/json"):
+    client = boto3.client("runtime.sagemaker")
+    response = client.invoke_endpoint(
+        EndpointName=endpoint_name, ContentType=content_type, Body=encoded_json
+    )
+    return response
+
+
+def parse_response_model_KoAlpaca(query_response):
+    model_predictions = json.loads(query_response["Body"].read())
+#    generated_text = model_predictions["generated_texts"]
+#    return generated_text
+    return model_predictions
+
+
+def parse_response_multiple_texts_bloomz(query_response):
+    generated_text = []
+    model_predictions = json.loads(query_response["Body"].read())
+    for x in model_predictions[0]:
+        generated_text.append(x["generated_text"])
+    return generated_text        
