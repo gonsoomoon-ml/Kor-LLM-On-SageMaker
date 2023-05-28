c69ef770fc46d00585f222793664bd6a113e3641
Gonsoomoon <gonsoomoon@gmail.com>
2023-05-28 05:37:30 +0000

    edit lib nb

--- common_code/inference_lib.py
+++ common_code/inference_lib.py
@@ -37,32 +37,95 @@ def invoke_inference(endpoint_name, prompt):
     print (eval(res)[0]['generated_text'])
 
             
-def query_endpoint_with_json_payload(encoded_json, endpoint_name, content_type="application/json"):
+        
+def query_endpoint_with_text_payload(plain_text, endpoint_name, content_type="text/plain"):
+    '''
+    content_type 이 text/plain 인 경우 사용
+    '''
     client = boto3.client("runtime.sagemaker")
     response = client.invoke_endpoint(
-        EndpointName=endpoint_name, ContentType=content_type, Body=encoded_json
-    )
-    return response
-        
-        
-def query_endpoint_with_json_payload(encoded_json, endpoint_name, content_type="application/json"):
-    client = boto3.client("runtime.sagemaker")
-    response = client.invoke_endpoint(
-        EndpointName=endpoint_name, ContentType=content_type, Body=encoded_json
+        EndpointName=endpoint_name, ContentType=content_type, Body=plain_text
     )
     return response
 
 
-def parse_response_model_KoAlpaca(query_response):
+def parse_response_text_model(query_response):
+    '''
+    content_type 이 text/plain 인 경우 사용
+    '''
+    
     model_predictions = json.loads(query_response["Body"].read())
-#    generated_text = model_predictions["generated_texts"]
-#    return generated_text
-    return model_predictions
+    # print("model_predictions: \n", model_predictions)
+    generated_text = model_predictions[0]["generated_text"]
+    return generated_text
+
+################################################
+# Embedding Handler
+################################################
+
+from langchain.embeddings.sagemaker_endpoint import EmbeddingsContentHandler
+from langchain.embeddings import SagemakerEndpointEmbeddings
+from langchain.llms.sagemaker_endpoint import ContentHandlerBase
+from typing import Any, Dict, List, Optional
+
+class SagemakerEndpointEmbeddingsJumpStart(SagemakerEndpointEmbeddings):
+    def embed_documents(self, texts: List[str], chunk_size: int = 5) -> List[List[float]]:
+        """Compute doc embeddings using a SageMaker Inference Endpoint.
+
+        Args:
+            texts: The list of texts to embed.
+            chunk_size: The chunk size defines how many input texts will
+                be grouped together as request. If None, will use the
+                chunk size specified by the class.
+
+        Returns:
+            List of embeddings, one for each text.
+        """
+        results = []
+        _chunk_size = len(texts) if chunk_size > len(texts) else chunk_size
+
+        print("text size: ", len(texts))
+        print("_chunk_size: ", _chunk_size)
+
+        for i in range(0, len(texts), _chunk_size):
+            response = self._embedding_func(texts[i : i + _chunk_size])
+            print
+            results.extend(response)
+        return results
+
+import numpy as np
+    
+class EmbeddingContentHandler(EmbeddingsContentHandler):
+    content_type = "application/json"
+    accepts = "application/json"
+
+    def transform_input(self, prompt: str, model_kwargs={}) -> bytes:
+        input_str = json.dumps({"inputs": prompt, **model_kwargs})
+        return input_str.encode("utf-8")
+
+    def transform_output(self, output: bytes) -> str:
+        response_json = json.loads(output.read().decode("utf-8"))
+        print("response_json shape: ", np.array(response_json).shape)
+        print("response_json shape: ", np.array(response_json[0][0][0]).shape)        
+        # print("response_json in EmbeddingContentHandler \n",  response_json)
+        # embeddings = response_json["embedding"][0]
+        embeddings = response_json[0][0]     
+        return embeddings
 
 
-def parse_response_multiple_texts_bloomz(query_response):
-    generated_text = []
-    model_predictions = json.loads(query_response["Body"].read())
-    for x in model_predictions[0]:
-        generated_text.append(x["generated_text"])
-    return generated_text        
+################################################
+# LLM  Handler
+################################################
+from langchain.llms.sagemaker_endpoint import LLMContentHandler
+
+class KorLLMContentHandler(LLMContentHandler):
+    content_type = "application/json"
+    accepts = "application/json"
+
+    def transform_input(self, prompt: str, model_kwargs={}) -> bytes:
+        input_str = json.dumps({"text_inputs": prompt, **model_kwargs})
+        return input_str.encode("utf-8")
+
+    def transform_output(self, output: bytes) -> str:
+        response_json = json.loads(output.read().decode("utf-8"))
+        return response_json["generated_texts"][0]
